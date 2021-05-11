54438ae7ca56359e365fb3eaa6edcadb5bec188d
Ben Silveston <bsilveston37@gmail.com>
2021-05-11 11:08:30 +0100

    Updated JSDOC comment for NPM section.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
  *
  * Locally, the above terminal command would instead be:
  *
- * npm i (install) <package-name>
+ * npm i ("i" stands for "install") <package-name>
  *
  * For access and simplicity, it is best to install all npm packages
  * locally.  This is so that these packages are only used in specific projects.
@@ -24,8 +24,8 @@
  * the latter is a manifest file that contains important project information.  The package.json
  * file is automatically installed when the following commands are run in the terminal:
  *
- * npm init - walk through all of the various properties within the file
- * npm init -y - all properties added by default.  These can be changed later.
+ * npm init --> walk through all of the various properties within the file
+ * npm init -y ("y" stands for "yes") --> all properties added by default.  These can be changed later.
  *
  * The package.json file can also be created manually in the project root folder.
  * Then hard-coding all packages manually within the file itself.
